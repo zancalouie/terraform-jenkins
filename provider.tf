1175394a6c85ba296d98f80110667c49589adc25
zancalouie <zancalouie@gmail.com>
2025-12-02 22:27:11 -0500

    3

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["/Users/zancal/.aws/credentials"]
+  shared_credentials_files = ["/Users/zanca/.aws/credentials"]
 }
