c562a7f830f584552597076607f61d86b8dfd37c
zancalouie <zancalouie@gmail.com>
2025-12-02 22:55:32 -0500

    idk

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
+    bucket = "dev-proj-1-jenkins-remote-state-bucket-zancal"
     key    = "devops-project-1/jenkins/terraform.tfstate"
     region = "us-east-1"
   }
